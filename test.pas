5ce9bd6d4abf0d518fdb290acdf4bacc4b72232a
DimitriTimoz <45998396+DimitriTimoz@users.noreply.github.com>
2022-06-01 15:58:29 +0200

    mise en place

--- test.pas
+++ test.pas
@@ -1,33 +1,15 @@
 program test;
 
-uses structures, Backend, crt;
+uses structures, backend, ihm;
 
-procedure afficher_plateau (partie : Jeu);
-var l,c : integer;
-begin
-	clrScr();
-	for l := 0 to partie.config.taille_grille - 1 do
-	begin
-		for c := 0 to partie.config.taille_grille - 1 do 
-		begin
-			if partie.liste_paires[l][c]^.decouverte = true then
-				write(partie.liste_paires[l][c]^.lettre)
-			else
-				write('#');
-		end;
-		writeln();
-	end;
-end;
 
 var
-    l_paires: PPPPaire;
-    config: Configuration;
     partie: Jeu;
 begin
-    config.taille_grille := 6;
-    partie.config := config;
-    creer_partie(l_paires, config);
-    partie.liste_paires := l_paires;
-    afficher_plateau(partie);
-    freeMem(l_paires);
+    initialisation(partie);
+    repeat
+        tour(partie);
+    until True;    
+    message_fin(partie);
+
 end.
